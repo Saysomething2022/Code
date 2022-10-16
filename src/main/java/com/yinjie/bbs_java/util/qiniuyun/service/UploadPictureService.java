9509fb0a8132f4c008e85d08a32816f0fd21a4db
LYN <314699890@qq.com>
2022-10-16 15:25:42 +0800

    更新

--- src/main/java/com/yinjie/bbs_java/util/qiniuyun/service/UploadPictureService.java
+++
@@ -1,9 +0,0 @@
-package com.yinjie.bbs_java.util.qiniuyun.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface UploadPictureService {
-    String uploadPicture(MultipartFile file) throws IOException;
-}
