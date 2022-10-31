5ccdaa46a3e57905e1b5698a6bf81b97c149e9b1
gc <92003120+wang990921@users.noreply.github.com>
2022-10-31 23:47:05 +0800

    Update Message.vue

--- src/components/admin/Message.vue
+++ src/components/admin/Message.vue
@@ -43,8 +43,8 @@
         <el-table ref="table" :data="messageList" height="400" @selection-change="selectChange">
           <el-table-column type="selection" width="50"></el-table-column>
           <el-table-column label="编号" fixed prop="id" width="150"></el-table-column>
-          <el-table-column label="消息源用户id" prop="oriId" width="250" show-overflow-tooltip="true"></el-table-column>
-          <el-table-column label="消息目标用户id" prop="desId" width="300" show-overflow-tooltip="true"></el-table-column>
+          <el-table-column label="消息源用户id" prop="oriId" width="250"></el-table-column>
+          <el-table-column label="消息目标用户id" prop="desId" width="300"></el-table-column>
           <el-table-column label="消息类型"
                            prop="mesType"
                            width="200"
