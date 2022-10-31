8d80325647d51f3ad67b4d00441aaf598c5a07ec
gc <92003120+wang990921@users.noreply.github.com>
2022-10-31 20:52:09 +0800

    Update Menu.vue

--- src/components/admin/Menu.vue
+++ src/components/admin/Menu.vue
@@ -63,7 +63,7 @@
           </el-menu-item>
         </router-link>
          
-      <--   <router-link to="/article" class="menu">
+      <!--   <router-link to="/article" class="menu">
           <el-menu-item index="8">
             <el-icon><setting /></el-icon>
             <template #title>返回首页</template>
